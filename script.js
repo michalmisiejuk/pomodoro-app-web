6dc73b4cd61f69a9c3eabb5f164c9a2827022342
michalmisiejuk <michalmisiejuk@gmail.com>
2021-09-18 00:55:04 +0200

    -sec disabled

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // "use strict";
+const breakMinutes = 5;
+const workMinutes = 25;
 
-const startingMinutes = 25;
-let time = startingMinutes * 60;
+let time = workMinutes * 60;
 
 const countdownEl = document.getElementById("countdown");
+const startStop = document
+  .getElementById("start-stop")
+  .addEventListener("click", startStopFuncion);
+
+function startStopFuncion() {
+  //TO DO!
+  //zamiana grafiki PLAY -> PAUSE
+  //zamiana napisu FOCUS -> PAUSE
+  //zatrzymanie funkcji ODLICZANIE na PAUZE
+}
 
 setInterval(updateCountdown, 1000);
 
@@ -11,6 +22,9 @@ function updateCountdown() {
   const minutes = Math.floor(time / 60);
   seconds = time % 60;
 
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
   countdownEl.innerHTML = `${minutes}: ${seconds}`;
   time--;
+  time = time < 0 ? 0 : time;
 }
