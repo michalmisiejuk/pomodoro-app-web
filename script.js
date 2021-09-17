5a2d1e1786a8edf53155adf96fc33827b3ea9df8
michalmisiejuk <michalmisiejuk@gmail.com>
2021-09-17 23:12:52 +0200

    Time Working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // "use strict";
+
+const startingMinutes = 25;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  seconds = time % 60;
+
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
